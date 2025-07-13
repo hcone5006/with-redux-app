219d2fef1fbc76166f174419ac0316ba5907e079
Heidi <heidi.cone@clearpoint.co.nz>
2025-07-14 11:47:45 +1200

    adding storybook

--- app/components/post/Post.tsx
+++ app/components/post/Post.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/stories/Button';
 import styles from './post.module.scss';
 
 export default function Post(props: any) {
@@ -5,14 +6,19 @@ export default function Post(props: any) {
   return (
     <div className={styles.post_card}>
       <h3 className={styles.post_title}>{props.title}</h3>
-      <h6>{props.id}</h6>
       <p className={styles.post_body}>{props.body}</p>
-      <button
+      {/* <button
         className={styles.btn_delete}
         onClick={() => {
           props.deletePost(props.id)
         }}
-      >Delete</button>
+      >Delete</button> */}
+      <Button 
+        label="Delete" 
+        onClick={() => {
+            props.deletePost(props.id)
+          }}
+      />
     </div>
   )
 }
